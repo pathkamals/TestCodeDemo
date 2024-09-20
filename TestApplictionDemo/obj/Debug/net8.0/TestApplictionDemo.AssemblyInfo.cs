0c619fb73817048677828e08344de0cf727ae01e
KamalSExl <100215617+KamalSExl@users.noreply.github.com>
2024-09-20 17:48:40 +0530

    register

--- TestApplictionDemo/obj/Debug/net8.0/TestApplictionDemo.AssemblyInfo.cs
+++ TestApplictionDemo/obj/Debug/net8.0/TestApplictionDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApplictionDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c89666f7f53c92e440164896b96971ce289b4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3b7affa1406b633bbe71d77148ff1b307b3a067")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApplictionDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApplictionDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
