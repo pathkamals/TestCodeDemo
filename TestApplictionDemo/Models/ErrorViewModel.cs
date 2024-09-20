22c89666f7f53c92e440164896b96971ce289b4e
pathkamals <kamaletah@gmail.com>
2024-09-20 17:19:10 +0530

    new project

---
+++ TestApplictionDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestApplictionDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
